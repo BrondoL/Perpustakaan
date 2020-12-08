f00945e70d94c62b22b73d26cc2a045ae79fe4d3
BrondoL <nabilunited2@gmail.com>
2020-12-08 11:38:40 +0700

    Index User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,9 +3,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-
     public function index()
     {
-        $this->load->view('welcome_message');
+        $data['title'] = 'Daftar Buku';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/daftarbuku', $data);
+        $this->load->view('templates/footer');
     }
 }
